1614674537472ebcb120211805c793aa85a94318
paulman <paul.mannarino@stonybrook.edu>
2020-06-06 13:04:20 -0400

    Added test data

--- src/store.js
+++ src/store.js
@@ -1,43 +1,67 @@
 import Vue from "vue";
 import Vuex from "vuex";
- 
+
 Vue.use(Vuex);
- 
+
 export default new Vuex.Store({
- state: {
-  selectedArtist: {
-    id: '',
-    name: ''
+  state: {
+    selectedArtist: {},
+    selectedVenue: {},
+    setlists: [],
+    testdata: {
+      ARTIST: {
+        mbid: "9311e2bc-bb3f-44cf-90d8-b1fe6912b60b",
+        name: "Brand New",
+        sortName: "Brand New",
+        disambiguation: "US alternative rock",
+        url: "https://www.setlist.fm/setlists/brand-new-4bd69b82.html",
+      },
+      VENUE: {
+        id: "4bd7e35a",
+        name: "The Paramount",
+        city: {
+          id: "5121636",
+          name: "Huntington",
+          state: "New York",
+          stateCode: "NY",
+          coords: {
+            lat: 40.868154,
+            long: -73.425676,
+          },
+          country: {
+            code: "US",
+            name: "United States",
+          },
+        },
+        url:
+          "https://www.setlist.fm/venue/the-paramount-huntington-ny-usa-4bd7e35a.html",
+      },
+    },
   },
-  selectedVenue: {
-    id: '',
-    name: ''
+  getters: {
+    selectedArtist: (state) => {
+      return state.selectedArtist;
+    },
+    selectedVenue: (state) => {
+      return state.selectedVenue;
+    },
+    setlists: (state) => {
+      return state.setlists;
+    },
+    testdata: (state) => {
+      return state.testdata;
+    },
   },
-  setlists: [],
- },
- getters: {
-  selectedArtist: state => {
-    return state.selectedArtist;
+  mutations: {
+    setSelectedArtist(state, payload) {
+      Vue.set(state, "selectedArtist", JSON.parse(JSON.stringify(payload)));
+    },
+    setSelectedVenue(state, payload) {
+      Vue.set(state, "selectedVenue", JSON.parse(JSON.stringify(payload)));
+    },
+    setSetlists(state, payload) {
+      Vue.set(state, "setlists", JSON.parse(JSON.stringify(payload)));
+    },
   },
-  selectedVenue: state => {
-    return state.selectedVenue;
-  },
-  setlists: state => {
-    return state.setlists;
-  },
- },
- mutations: {
-  setSelectedArtist (state, payload) {
-    state.selectedArtist.id = payload.artistId;
-    state.selectedArtist.name = payload.artistName;
-  },
-  setSelectedVenue (state, payload) {
-    state.selectedVenue.id = payload.venueId;
-    state.selectedVenue.name = payload.venueName;
-  },
-  setSetlists (state, payload) {
-    Vue.set(state, 'setlists', payload);
-  }
- },
- actions: {}
+  actions: {},
 });
