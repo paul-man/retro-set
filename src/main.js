2afe7281c0115deae984eeeef99cfa9ee06ac6de
paulman <paul.mannarino@stonybrook.edu>
2020-06-07 18:48:23 -0400

    Importing panel dependancy

--- src/main.js
+++ src/main.js
@@ -5,15 +5,17 @@ import store from "./store"
 import "./filters"
 import axios from 'axios'
 import VueAxios from 'vue-axios'
-import VueBootstrapTypeahead from 'vue-bootstrap-typeahead';
+import VueBootstrapTypeahead from "vue-bootstrap-typeahead";
+import VueSlideoutPanel from "vue2-slideout-panel";
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import TrackSelect from '@/components/TrackSelect'
 
 Vue.config.productionTip = false;
-
 Vue.use(VueAxios, axios);
-
-Vue.component('vue-bootstrap-typeahead', VueBootstrapTypeahead)
+Vue.use(VueSlideoutPanel);
+Vue.component('track-select', TrackSelect);
+Vue.component('vue-bootstrap-typeahead', VueBootstrapTypeahead);
 
 new Vue({
   router,
