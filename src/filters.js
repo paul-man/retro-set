5755b5a12f3817187cf1babb96f1979d7d88a577
paulman <paul.mannarino@stonybrook.edu>
2020-06-07 18:46:47 -0400

    New date filter

--- src/filters.js
+++ src/filters.js
@@ -1,4 +1,8 @@
 import Vue from "vue"
 
 Vue.filter("stringify", str => JSON.stringify(str, null, 2))
-Vue.filter("pretty", str => JSON.stringify(JSON.parse(str), null, 2))
+Vue.filter("prettyDate", (date) =>  {
+  date = date.split('-')
+  date = new Date(date[2], date[0], - 1, date[1])
+  return date.toDateString()
+});
