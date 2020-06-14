00393193e9eb2b47cbc21f49e346e7600623e398
paulman <paul.mannarino@stonybrook.edu>
2020-06-14 19:27:02 -0400

    Updating imports

--- src/main.js
+++ src/main.js
@@ -3,18 +3,14 @@ import App from './App.vue'
 import router from './router'
 import store from "./store"
 import "./filters"
-import axios from 'axios'
-import VueAxios from 'vue-axios'
-import VueBootstrapTypeahead from "vue-bootstrap-typeahead";
-import VueSlideoutPanel from "vue2-slideout-panel";
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import TrackSelect from '@/components/TrackSelect'
+import VueBootstrapTypeahead from 'vue-bootstrap-typeahead'
+import VueSlideoutPanel from 'vue2-slideout-panel'
 
-Vue.config.productionTip = false;
-Vue.use(VueAxios, axios);
-Vue.use(VueSlideoutPanel);
+// Registering globally for usage with VueSlideoutPanel
+import TrackSelect from '@/components/TrackSelect'
 Vue.component('track-select', TrackSelect);
+
+Vue.use(VueSlideoutPanel);
 Vue.component('vue-bootstrap-typeahead', VueBootstrapTypeahead);
 
 new Vue({
