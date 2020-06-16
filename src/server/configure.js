1dfc4d3436823b05622684b9abe1675e26289bcf
paulman <paul.mannarino@stonybrook.edu>
2020-06-15 23:26:47 -0400

    Moved mongo initialization inside export

--- src/server/configure.js
+++ src/server/configure.js
@@ -1,14 +1,14 @@
 const api = require('./api')
 let mongoUtil = require('./mongoUtil');
 
-mongoUtil.connectToServer( function(err) {
-  if (err) { console.log(err); }
-  else {
-    console.log('MONGO CONNECTED');
-    return;
-  }
-});
-
 module.exports = app => {
   app.use('/api', api)
+  
+  mongoUtil.connectToServer( function(err) {
+    if (err) { console.log(err); }
+    else {
+      console.log('MONGO CONNECTED');
+      return;
+    }
+  });
 }
