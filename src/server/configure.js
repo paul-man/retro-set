329a5d3b15e0c0aea79502e4eefa9cd63c29a60a
paulman <paul.mannarino@stonybrook.edu>
2020-06-15 23:22:05 -0400

    Returning after mongo connection

--- src/server/configure.js
+++ src/server/configure.js
@@ -2,8 +2,11 @@ const api = require('./api')
 let mongoUtil = require('./mongoUtil');
 
 mongoUtil.connectToServer( function(err) {
-  if (err) console.log(err);
-  else console.log('MONGO CONNECTED')
+  if (err) { console.log(err); }
+  else {
+    console.log('MONGO CONNECTED');
+    return;
+  }
 });
 
 module.exports = app => {
