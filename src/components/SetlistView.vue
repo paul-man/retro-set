9898021ecfbffca124819daeec9e69516eb75542
paulman <paul.mannarino@stonybrook.edu>
2020-06-23 00:02:51 -0400

    Adding setlists collapse

--- src/components/SetlistView.vue
+++ src/components/SetlistView.vue
@@ -3,27 +3,34 @@
     <div class="container">
       <div class="row">
         <div class="col-sm-5 col-md-6 setlist-container" v-for="(set, index) in setlists" :key="index">
-          <p class="setData">
-            Event date: {{ set.eventDate }}<br>
-            Songs played: {{ set.songs.length }}<br>
-            <a :href="set.url" target="_blank">More setlist Data</a>
-          </p>
-          <template v-if="set.tourName">{{ set.tourName }}</template>
-          <b-button variant="primary" @click="openTrackSelctPanel(set, index)">Search Songs</b-button><br><br>
-          <template v-if="set.spotifyPreviews">
-            <div class="container">
-              <div v-for="preview in set.spotifyPreviews" v-bind:key="preview" class="row">
-                <div class="spotify-widget-container" v-html="preview.html"></div>
+          <b-container class="float-right" style="text-align:right; padding-top:1em;">
+            <b-button v-b-toggle="'set-collapse-'+index" variant="outline-primary">
+              <b-icon-plus></b-icon-plus>
+            </b-button>
+          </b-container>
+          <b-collapse :id="'set-collapse-'+index" visible class="mt-2">
+            <p class="setData">
+              Event date: {{ set.eventDate }}<br>
+              Songs played: {{ set.songs.length }}<br>
+              <a :href="set.url" target="_blank">More setlist Data</a>
+            </p>
+            <template v-if="set.tourName">{{ set.tourName }}</template>
+            <b-button variant="primary" @click="openTrackSelctPanel(set, index)">Search Songs</b-button><br><br>
+            <template v-if="set.spotifyPreviews">
+              <div class="container">
+                <div v-for="preview in set.spotifyPreviews" v-bind:key="preview" class="row">
+                  <div class="spotify-widget-container" v-html="preview.html"></div>
+                </div>
               </div>
-            </div>
-          </template>
-          <template v-else-if="set.songs">
-            <b-list-group class="songlist">
-              <b-list-group-item v-for="(song, index) in set.songs" :key="index">
-                {{ song.name }}
-              </b-list-group-item>
-            </b-list-group>
-          </template>
+            </template>
+            <template v-else-if="set.songs">
+              <b-list-group class="songlist">
+                <b-list-group-item v-for="(song, index) in set.songs" :key="index">
+                  {{ song.name }}
+                </b-list-group-item>
+              </b-list-group>
+            </template>
+          </b-collapse>
         </div>
       </div>
     </div>
@@ -78,13 +85,10 @@ export default {
           visibility: set.playlistVisibility
         }
       });
-      // if (res.data.error) {
-      //   this.trackSearchError = true;
-      //   this.trackSearchErrorCode = spotifyResp.data.status;
-      //   return {
-      //     warning: "Unable to retrieve resutls",
-      //   };
-      // }
+      if (res.error) {
+        this.makeErrorToast('Can\'t create playlist at this time');
+      }
+      
       set.spotifyPreviews = [ { html: `<iframe src="https://open.spotify.com/embed/playlist/${res.data.id}" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>` } ]
       this.setlists[index] = set;
       this.$store.commit('setSetlists', this.setlists);
@@ -99,7 +103,7 @@ export default {
 
  /* Add border between mutliple setlists */
 .setlist-container {
-  border: solid 1px lightgray;
+  border: solid 1px rgba(211, 211, 211, 0.459);
   border-radius: 2px;
   padding-bottom: 15px;
 }
