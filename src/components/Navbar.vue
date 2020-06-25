e58dd731e2af1b799aa9447bb699c1f8395b79ab
paulman <paul.mannarino@stonybrook.edu>
2020-06-25 00:31:39 -0400

    Updating styling

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,31 +1,38 @@
 <template>
   <!-- <div id="nav"> -->
-    <b-navbar toggleable="lg" type="light" variant="light" sticky>
+  <b-navbar toggleable="lg" type="light" variant="light" sticky>
     <b-navbar-brand>
-      <img
-        src="@/assets/icon.png"
-        id="retroset-icon"
-        height="30"
-        width="30"
-      />
-      RetroSet {{(isDev ? ': Dev' : '')}}
+      <img src="@/assets/icon.png" id="retroset-icon" height="30" width="30" />
+      RetroSet
     </b-navbar-brand>
-    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+    <b-navbar-toggle target="nav-collapse">
+      <template v-slot:default="{ expanded }">
+        <b-icon v-if="expanded" variant="primary" icon="dash-square"></b-icon>
+        <b-icon v-else variant="primary" icon="plus-square"></b-icon>
+      </template>
+    </b-navbar-toggle>
     <b-collapse id="nav-collapse" is-nav>
       <!-- Right aligned nav items -->
       <b-navbar-nav class="ml-auto">
         <b-nav-item-dropdown right>
           <!-- Using 'button-content' slot -->
           <template v-slot:button-content>
-              <img
-                :src="user.imgUrl"
-                class="d-inline-block align-top"
-                id="spotify-user-img"
-                alt="Spotify user image"
-              />{{ user.id }}
+            <img
+              :src="user.imgUrl"
+              class="d-inline-block align-top"
+              id="spotify-user-img"
+              alt="Spotify user image"
+            />{{ user.id }}
           </template>
-          <b-dropdown-item :href="'https://open.spotify.com/user/' + user.id" target="_blank">Spotify Profile</b-dropdown-item>
-          <b-dropdown-item @click="logout" title="Switch spotify account">Not you?</b-dropdown-item>
+          <b-dropdown-item
+            :href="'https://open.spotify.com/user/' + user.id"
+            target="_blank"
+            >Spotify Profile</b-dropdown-item
+          >
+          <b-dropdown-divider></b-dropdown-divider>
+          <b-dropdown-item @click="logout" title="Switch spotify account"
+            >Not you?</b-dropdown-item
+          >
         </b-nav-item-dropdown>
       </b-navbar-nav>
     </b-collapse>
@@ -33,28 +40,32 @@
 </template>
 
 <script>
-import { mapState } from 'vuex'
-import { get } from 'axios'
+import { mapState } from "vuex";
+import { get } from "axios";
 export default {
   name: "Navbar",
   data() {
-    return {
-    };
+    return {};
   },
+
+  mounted() {
+  },
+
   components: {},
+
   computed: {
-    ...mapState(['user']),
+    ...mapState(["user"]),
     isDev() {
-      return process.env.NODE_ENV.trim() === 'development'
-    }
+      return process.env.NODE_ENV.trim() === "development";
+    },
   },
   methods: {
     async logout() {
-      this.$store.commit("setUser", {})
+      this.$store.commit("setUser", {});
       let res = await get("api/spotify/login/", { params: { newUser: true } });
-      window.location = res.data
-    }
-  }
+      window.location = res.data;
+    },
+  },
 };
 </script>
 
@@ -62,6 +73,14 @@ export default {
 nav {
   padding: 0;
   border-bottom: lightgray solid 1px;
+}
+
+#nav-collapse.show {
+  border-top: 1px solid rgba(211, 211, 211, 0.562);
+}
+
+.navbar-nav.ml-auto {
+  width: 10em !important;
 }
 
 #retroset-icon {
