c284f8a95e1a94a905a9395c512ecef33f7fccf9
paulman <paul.mannarino@stonybrook.edu>
2020-07-05 15:47:06 -0400

    Using promises for non-blocking data fetching

--- src/components/TrackSelect.vue
+++ src/components/TrackSelect.vue
@@ -1,7 +1,15 @@
 <template>
-  <div id="track-select" :key="componentKey">
+  <div id="track-select" :key="componentKey" ref="trackSelect">
     <b-container class="rounded" id="playlist-header">
-      <h4>Confirm matches and create playlist</h4>
+      <p
+        class="d-inline-block d-none d-md-block d-sm-block spotify-user-id">
+        Playlist for 
+        <a
+          :href="'https://open.spotify.com/user/' + user.id"
+          target="_blank">
+            {{ user.id }}
+          </a>
+      </p>
     </b-container>
     <div id="playlist-data-wrapper">
       <b-container id="playlist-form">
@@ -49,6 +57,15 @@
           </b-form-radio>
         </b-form-group>
       </b-container>
+      <b-container id="search-btn-desc">
+        Use 
+        <b-button
+          size="sm"
+          class="mb-2 add-song-btn">
+          <b-icon icon="music-note" aria-hidden="true"></b-icon>+
+        </b-button> 
+        to search and add more song matches
+      </b-container>
       <table
         class="table table-striped table-bordered table-sm"
         id="matches-table">
@@ -75,68 +92,66 @@
                 </div>
               </template>
               <template v-else>
-                <div class="add-match-btn-div">
-                  <b-button
-                    size="sm"
-                    class="mb-2"
-                    title="Search Spotify for song"
-                    @click="openTrackSearchModal(song)">
-                    <b-icon icon="music-note" aria-hidden="true"></b-icon>+
-                  </b-button>
-                </div>
-                <div v-if="song.matches.length === 0" class="no-match-warn">
-                  <p>
-                    No matches found for "{{ song.name }}" by
-                    {{ selectedArtist.name }}
-                  </p>
-                  <p>
-                    For now you'll have to add the song yourself at position
-                    {{ songIndex + 1 }}
-                  </p>
-                  <p>
-                    Checkout the
-                    <a :href="set.url" target="_blank">setlist</a> for more
-                    information
-                  </p>
-                </div>
-                <div
-                  v-else
-                  :class="'container matches-wrapper' + (song.matches.length > 1 ? ' shadow rounded multiple' : '')">
-                  <div
-                    class="row match-div"
-                    v-for="(match, matchIndex) in song.matches"
-                    :key="matchIndex"
-                  >
-                    {{ setDefaultMatch(song, match.uri, matchIndex) }}
-                    <b-form-radio
-                      variant="primary"
-                      v-model="song.selectedUri"
-                      :value="match.uri"
-                      :name="'match-' + songIndex"
-                    />
-                    <p style="float: left;">
-                      <img
-                        :src="getAlbumImg(match.albumImageUrl)"
-                        height="64px"
-                        width="64px"
-                        border="1px"
-                      />
-                    </p>
-                    <div
-                      class="float-right match-desc"
-                      style="text-align:left;">
+                <b-row>
+                  <b-col sm="12">
+                    <div v-if="song.matches.length === 0" class="no-match-warn">
                       <p>
-                        <span class="bold">Title:</span> {{ match.songTitle}}
-                      </p><br/>
+                        No matches found for "{{ song.name }}" by
+                        {{ selectedArtist.name }}. Search for a replacement track or add it later at position
+                        {{ songIndex + 1 }}.
+                      </p>
                       <p>
-                        <span class="bold">Artist:</span> {{ match.artistName}}
-                      </p><br/>
-                      <p>
-                        <span class="bold">Album:</span> {{ match.albumTitle }}
+                        Checkout the
+                        <a :href="set.url" target="_blank">setlist</a> for more
+                        information
                       </p>
                     </div>
-                  </div>
-                </div>
+                    <div
+                      v-else
+                      :class="'container matches-wrapper' + (song.matches.length > 1 ? ' shadow rounded multiple' : '')">
+                      <div
+                        class="row match-div"
+                        v-for="(match, matchIndex) in song.matches"
+                        :key="matchIndex">
+                        {{ setDefaultMatch(song, match.uri, matchIndex) }}
+                        <b-form-radio
+                          variant="primary"
+                          v-model="song.selectedUri"
+                          :value="match.uri"
+                          :name="'match-' + songIndex"/>
+                        <p style="float: left;">
+                          <img
+                            :src="getAlbumImg(match.albumImageUrl)"
+                            class="img-md"/>
+                        </p>
+                        <div
+                          class="float-right match-desc"
+                          style="text-align:left;">
+                          <p>
+                            <span class="bold">Title:</span> {{ match.songTitle}}
+                          </p>
+                          <p>
+                            <span class="bold">Artist:</span> {{ match.artistName}}
+                          </p>
+                          <p>
+                            <span class="bold">Album:</span> {{ match.albumTitle }}
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </b-col>
+                </b-row>
+                <b-row>
+                  <b-col>
+                    <b-button
+                      size="sm"
+                      class="mb-2 add-song-btn"
+                      title="Search Spotify for song"
+                      @click="openTrackSearchModal(song)">
+                      <b-icon icon="music-note" aria-hidden="true"></b-icon>+
+                    </b-button>  
+                  </b-col>
+                </b-row>
               </template>
             </td>
           </tr>
@@ -152,13 +167,14 @@
       <spotify-track-search />
     </b-modal>
     <div class="panel-footer rounded">
-      <b-button @click="closePanel">Cancel</b-button>
+      <b-button @click="closePanel" size="sm">Cancel</b-button>
       <b-button
         variant="primary"
         @click="refreshSetlists"
         id="create-playlist-btn"
-        >Create Playlist</b-button
-      >
+        size="sm">
+        Create Playlist <i class="fa fa-spotify"></i>
+      </b-button>
     </div>
   </div>
 </template>
@@ -187,8 +203,7 @@ export default {
     this.set.playlistVisibility = "private";
     this.set.playlistDescription = "";
     for (let song of this.set.songs) {
-      let matches = await this.trackSearch(song);
-      song.matches = matches;
+      this.trackSearch(song);
     }
     if (this.trackSearchError) {
       this.makeErrorToast(this.respCodes[this.trackSearchErrorCode]);
@@ -197,15 +212,10 @@ export default {
   },
 
   computed: {
-    ...mapState(["selectedArtist", "selectedVenue", "respCodes"]),
+    ...mapState(["selectedArtist", "selectedVenue", "respCodes", "user"]),
     defaultPlaylistName() {
       return (
-        this.selectedArtist.name +
-        " - " +
-        this.selectedVenue.name +
-        " (" +
-        this.set.eventDate +
-        ")"
+        this.selectedArtist.name + " - " + this.selectedVenue.name + " (" + this.set.eventDate + ")"
       );
     },
   },
@@ -213,18 +223,22 @@ export default {
   methods: {
     async trackSearch(song) {
       song.matches = [];
-      const spotifyResp = await get("api/spotify/track/", {
+      get("api/spotify/track/", {
         params: {
           track: song.name,
           artist: this.selectedArtist.name,
         },
+      }).then( result => {
+        if (!result.data.error) {
+          song.matches = result.data
+          this.forceRerender()
+        } else {
+          this.trackSearchError = true;
+          this.trackSearchErrorCode = result.data.status;
+        }
+      }).catch( error => {
+        this.makeErrorToast(error);
       });
-      if (spotifyResp.data.error) {
-        this.trackSearchError = true;
-        this.trackSearchErrorCode = spotifyResp.data.status;
-        return [];
-      }
-      return spotifyResp.data;
     },
     setDefaultMatch(song, uri, index) {
       if (index === 0) {
@@ -253,6 +267,7 @@ export default {
     },
     forceRerender() {
       this.componentKey += 1;
+      console.log(this.componentKey)
     },
     makeToast(msg) {
       this.$bvToast.toast(msg, {
@@ -270,6 +285,7 @@ export default {
 <style lang="scss" scoped>
 #track-select {
   height: 100%;
+  text-align: left;
 }
 
 .add-match-btn-div {
@@ -282,13 +298,19 @@ export default {
   z-index: 1022 !important;
 }
 
+.add-song-btn {
+  margin-top: 0.5em;
+  background-color: $retro-green;
+  color: #2156D9;
+  border: 0;
+}
+
 .match-desc {
   overflow-x: auto;
   white-space: nowrap;
   display: inline-block;
 }
 .match-desc > p {
-  display: inline-block;
   float: none;
   margin-bottom: 0;
 }
@@ -301,6 +323,10 @@ tr.row {
 
 .jsonView {
   text-align: left;
+}
+
+h5 {
+  margin-bottom: 0 !important;
 }
 
 .form-check {
@@ -326,10 +352,6 @@ tr.row {
   border-bottom: solid 1px #e9e9e9;
 }
 
-.song-name {
-  color: $primary-shade;
-}
-
 .match-div {
   padding-left: 0.5em;
   padding-top: 0.2em;
@@ -338,21 +360,10 @@ tr.row {
 .matches-wrapper img {
   margin-right: 15px;
 }
-.panel-footer button {
-  margin-right: 10px;
-  margin-left: 10px;
-}
-
-.panel-footer {
-  padding-right: 25px;
-  padding-top: 8px;
-  text-align: right;
-  background-color: #e9e9e994;
-  height: 7%;
-}
 
 .no-match-warn {
   background-color: #fab0862d;
+  border: 2px solid #fab086;
   border-radius: 5px;
   padding: 5px 0px 1px 7px;
 }
@@ -375,17 +386,45 @@ tr:nth-child(even) {
 }
 
 #playlist-form {
-  text-align: left;
   padding-top: 1em;
+  margin: 0;
 }
 
 #playlist-data-wrapper {
-  max-height: 89%;
+  max-height: 90%;
   overflow-y: scroll;
 }
 
 #playlist-header {
   background-color: #e9e9e994;
-  height: 4%;
+  max-height: 5%;
+  padding: 5px 0px 5px 5px;
+  margin: 0;
+}
+
+.panel-footer button {
+  margin-right: 10px;
+  margin-left: 10px;
+}
+
+.panel-footer {
+  padding-right: 25px;
+  padding-top: 2px;
+  text-align: right;
+  background-color: #e9e9e994;
+  height: 5%;
+}
+
+#search-btn-desc {
+  text-align: center;
+  border-top: 1px solid rgba(211, 211, 211, 0.301);
+}
+
+#search-btn-desc button {
+  pointer-events: none;
+}
+
+.spotify-user-id {
+  font-size: large;
 }
 </style>
